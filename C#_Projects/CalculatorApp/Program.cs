438dd134a98d45235a312a32387c289b7cd34c0e
Sufyan Alikhan <sufyannalikhan@gmail.com>
2025-05-28 15:19:52 -0400

    #Added My c# Projects

---
+++ C#_Projects/CalculatorApp/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+
+namespace Calculator
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            bool keepRunning = true;
+
+            while (keepRunning)
+            {
+
+                Console.WriteLine("What would you like do today choose 1-4 to to an operation of two numbers");
+                Console.WriteLine(" ");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplcation");
+                Console.WriteLine("4. Division");
+                Console.WriteLine(" ");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Addition();
+                        break;
+
+                    case 2:
+                        Subtraction();
+                        break;
+                    case 3:
+                        Multiplcation();
+                        break;
+                    case 4:
+                        Division();
+                        break;
+
+                    default:
+                        Console.WriteLine("IVALID");
+                        break;
+                }
+                
+                Console.WriteLine("Would you like to do anything else type in yes or no");
+                string? response = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(response) || response.ToLower() != "yes")
+                {
+                    keepRunning = false;
+                    Console.WriteLine("Bye bye!");
+                }
+            }
+        }
+        public static void Addition()
+        {
+            Console.WriteLine("Pick number 2 numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int answer = num1 + num2;
+            Console.WriteLine($"Your asnwer is {answer} !");
+        }
+        public static void Subtraction()
+        {
+            Console.WriteLine("Pick number 2 numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int answer = num1 - num2;
+            Console.WriteLine($"Your asnwer is {answer} !");
+        }
+        public static void Multiplcation()
+        {
+            Console.WriteLine("Pick number 2 numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int answer = num1 * num2;
+            Console.WriteLine($"Your asnwer is {answer} !");
+        }
+        public static void Division()
+        {
+            Console.WriteLine("Pick number 2 numbers");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            double num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num2 == 0)
+            {
+                Console.WriteLine("CANNOT DIVIDE BY 0");
+                return;
+            }
+            double answer = num1 / num2;
+            Console.WriteLine($"Your asnwer is {answer} !");
+        }
+    }
+}
---
+++ C#_Projects/GuessTheNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GuessTheNumber
+{    public class GuessTheNumber
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! What is the range you want to Guess a number from ");
+
+            int min = int.Parse(Console.ReadLine());
+            int max = int.Parse(Console.ReadLine());
+
+            if (min >= max)
+            {
+                Console.WriteLine("The minimum number cannot be greater than the maximum number");
+                return;
+            }
+
+            Random rand = new Random();
+            int range = rand.Next(min, max);
+            int gueses = 0;
+            int attempts = 5;
+
+            Console.WriteLine(range);
+            Console.WriteLine($"Guess a number between {min} and {max}");
+            Console.WriteLine($"You have {attempts} attempts! ");
+
+            while (gueses != attempts)
+            {
+
+                int inputguess = int.Parse(Console.ReadLine());
+                gueses++;
+                int left = attempts - gueses;
+
+                if (inputguess == range)
+                {
+                    Console.WriteLine("YES!");
+                    return;
+                }
+                else if (inputguess != range)
+                {
+                    Console.WriteLine("Not Right");
+                    Console.WriteLine($"You have {left} attempts left! ");
+                }
+            }
+        }   
+    }
+}
